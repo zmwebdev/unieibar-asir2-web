d3931434a3a627f9ce41977243b53f8629e04323
zmwebdev@gmail.com <zmwebdev@gmail.com>
2018-09-25 09:35:43 +0200

    m

--- horario-with-arrays.php
+++ horario-with-arrays.php
@@ -1,39 +1,25 @@
 <?php
-
-// http://php.net/manual/en/function.date.php
-$day = date("N");
-$hour = date("H");
-
 // two dimensional array
 // https://www.w3schools.com/php/php_arrays_multi.asp<?php
 // https://www.w3schools.com/php/php_arrays.asp
 // https://www.w3schools.com/php/php_arrays_multi.asp
 
-// class schedule
-$schedule = array(
-    1=>array(8=>"A", 9=>"B",10=>"C",11=>"D",12=>"D",13=>"D",14=>"D"),
-    2=>array(8=>"A", 9=>"B",10=>"C",11=>"D",12=>"D",13=>"D",14=>"D"),
-    3=>array(8=>"A", 9=>"B",10=>"C",11=>"D",12=>"D",13=>"D",14=>"D"),
-    4=>array(8=>"A", 9=>"B",10=>"C",11=>"D",12=>"D",13=>"D",14=>"D"),
-    5=>array(8=>"A", 9=>"B",10=>"C",11=>"D",12=>"Z",13=>"Z",14=>"Z"),
+$horario = array(
+    array("WEB","WEB","SIST","SIST","RED","RED"),  // lunes 0
+    array("WEB","RED","WEB","WEB","SIST","SIST"),  // martes 1
+    array("WEB","WEB","SIST","SIST","RED","RED"),  // miercoles 2
+    array("WEB","WEB","SIST","SIST","RED","RED"),  // jueves 3
+    array("WEB","WEB","SIST","SIST","RED","RED")  // viernes 4
 );
 
 // http://php.net/manual/en/function.date.php
-$day = date("N");
-$hour = date("H");
 
-echo $day . "-" . $hour . "<br>";
+$dia = date("N");
+$hora = date("G");
 
-//print_r($schedule);
-//echo "monday 8:00  " . $schedule[1][1]; // monday 8:00;
+//echo "hora : $hora";
+// falta por controlar los horarios de fuera de dia y hora
+// if ...
+// Where can we check PHP errors?
 
-echo $schedule[$day][$hour];
-
-// function to print the all schedule table
-// TODO
-
-
-// error out of range
-// tail /var/log/apache2/error.log
-// how to check errors? windows?
-echo $schedule[$day][20];
+echo $horario[$dia-1][$hora-8];
