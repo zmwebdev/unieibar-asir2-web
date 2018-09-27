e56497682e9322aa25c5abdc8df561a58d3c8bbe
Aitor Agirre <AAgirre@uni.lan>
2018-09-27 13:38:28 +0200

    m

--- array-basico.php
+++ array-basico.php
@@ -4,6 +4,7 @@ $v = [1,5,6,99,12,11,111];
 
 //echo $suma = $v[0] + $v[1];
 
+// for
 $suma = 0;
 echo "suma: " . $suma . "<br>";
 echo "for....<br>";
@@ -15,4 +16,31 @@ for ($i=0; $i < count($v); $i++) {
 }
 echo "...for<br>";
 
-echo $suma;
+echo "$suma <br><br>";
+ 
+// foreach
+
+$suma = 0;
+foreach ($v as $x) {
+    echo "x: $x; suma: $suma <br>";
+    $suma = $suma + $x;
+}
+echo "Suma con foreach: $suma <br>";
+
+
+// Dos dimensiones
+
+$w = 
+[
+    [688,1,6,66,221],
+    [1,-12],
+    [-4,0,66,666,4,3]    
+];
+
+$suma = 0;
+foreach ($w as $v) {
+    foreach ($v as $x) {
+        $suma = $suma + $x;
+    }
+}
+echo "Suma de array de dos dim: $suma";
