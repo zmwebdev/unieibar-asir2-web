d1e0d3ff7eaa900bcfbfebe00c2e510740d9b402
Aitor Agirre <AAgirre@uni.lan>
2018-10-01 13:31:51 +0200

    horario print

--- horario-with-arrays.php
+++ horario-with-arrays.php
@@ -13,14 +13,24 @@ $horario = array(
 );
 // OR
 $horario = [
-    ["WEB","WEB","SIST","SIST","RED","RED"],  // lunes 0
+    ["DB","DB","SIST","SIST","WEB","WEB"],  // lunes 0
     ["WEB","RED","WEB","WEB","SIST","SIST"],  // martes 1
     ["WEB","WEB","SIST","SIST","RED","RED"],  // miercoles 2
     ["WEB","WEB","SIST","SIST","RED","RED"],  // jueves 3
     ["WEB","WEB","SIST","SIST","RED","RED"]  // viernes 4
 ];
 
+$horario = [
+    ["DB","DB","SIST","SIST","WEB"],  // 8-9    
+    ["DB","DB","SIST","SIST","WEB"],  // 9-10
+    ["WEB","RED","WEB","WEB","SIST"],  // 10-11
+    ["WEB","WEB","SIST","SIST","RED"],  // 11-12
+    ["WEB","WEB","SIST","SIST","RED"],  // 12-13
+    ["WEB","WEB","SIST","SIST","RED"]  // 13-14
+];
 
+
+/*
 // OR with Associative Arrays
 $horario2 = array(
     "Lunes" => array("WEB","WEB","SIST","SIST","RED","RED"),  // lunes 0
@@ -29,7 +39,7 @@ $horario2 = array(
     "Jueves" => array("WEB","WEB","SIST","SIST","RED","RED"),  // jueves 3
     "Viernes" => array("WEB","WEB","SIST","SIST","RED","RED")  // viernes 4
 );
-
+*/
 
 
 // http://php.net/manual/en/function.date.php
@@ -37,9 +47,38 @@ $horario2 = array(
 $dia = date("N");
 $hora = date("G");
 
-//echo $horario[$dia-1][$hora-8];
+//echo $horario[$hora-8][$dia-1] . "<br><br>";
 
-echo $horario2["Lunes"][0];
+// print horario
+echo "<table border='1'>";
+foreach ($horario as $dia) {
+    echo "<tr>";
+    foreach ($dia as $asig) {
+        echo "<td>";
+        echo $asig . "<br>";
+        echo "</td>";
+    }
+    echo "<tr>";
+}
+echo "</table>";
+
+// using for
+// print horario
+echo "<br><br>";
+echo "<table border='1'>";
+for ($i=0; $i<count($horario); $i++) {
+    echo "<tr>";
+    for ($j=0; $j<count($horario[$i]); $j++) {
+        echo "<td>";
+        echo $horario[$i][$j] . "<br>";
+        echo "</td>";
+    }
+    echo "<tr>";
+}
+echo "</table>";
+
+
+//echo $horario2["Lunes"][0];
 
 // Controlar los horarios de fuera de dia y hora
 // Print el horario
