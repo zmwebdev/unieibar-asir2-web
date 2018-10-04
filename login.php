b8f53af3a7eb36c5c868a77b28d0adf50ae0be9d
Aitor Agirre <AAgirre@uni.lan>
2018-10-04 13:57:11 +0200

    login

--- login.php
+++ login.php
@@ -2,10 +2,26 @@
 
 // Read from the formulary (login.html)
 // user / password
-
+$user = $_POST["user"];
+$password = $_POST["password"];
 
 // connect to mysql
+// http://php.net/manual/es/mysqli.construct.php
+// https://www.w3schools.com/php/php_mysql_connect.asp
 
+$servername = "localhost:3307";
+$username = "dbuser";
+$password = "dbuser";
+$db = "web";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 
 // select from user where ....
 
