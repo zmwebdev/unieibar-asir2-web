44d1a58aee894fb7a03a8ca972d9b5e9c97636f2
Aitor Agirre <AAgirre@uni.lan>
2018-10-04 12:43:37 +0200

    login

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+
+// Read from the formulary (login.html)
+// user / password
+
+
+// connect to mysql
+
+
+// select from user where ....
+
+// if ok go to loginok.html
+
+// if not ok return to login.html
