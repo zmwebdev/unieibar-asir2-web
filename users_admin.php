dd71c78d1ad137f93f4b8902491823b97d844ac9
Aitor Agirre <AAgirre@uni.lan>
2018-10-18 12:24:02 +0200

    admin

--- users_admin.php
+++ users_admin.php
@@ -24,20 +24,47 @@ $query = "SELECT name FROM users";
 $result = mysqli_query($conn, $query);
 
 /* numeric array */
+?>
+<html>
+<head>
+<style>
+   /* https://www.w3schools.com/css/tryit.asp?filename=trycss_table_striped */ 
+   table {
+    border-collapse: collapse;
+    width: 100%;
+    }
+
+    th, td {
+        text-align: left;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {background-color: #f2f2f2;}   
+</style>
+</head>
+<table border=0>
+<?php
 while($row = mysqli_fetch_array($result)){
     //echo $row[0] . "<br>";
 ?>
-
-    <?php echo $row[0] ?>:<br>
-    <form action="deleteuser.php" method="POST">
-        <input type="hidden" name="user" value="<?php echo $row[0] ?>">
-        <input type="submit" value="delete">
-    </form> 
-    <form action="updateuser.php" method="POST">
-        <input type="hidden" name="user" value="<?php  echo$row[0] ?>">
-        <input type="submit" value="update">
-    </form> 
-
+    <tr>
+    <td>
+        <?php echo $row[0] ?>:
+    </td>
+    <td>
+        <form action="deleteuser.php" method="POST">
+            <input type="hidden" name="user" value="<?php echo $row[0] ?>">
+            <input type="submit" value="delete">
+        </form>
+    </td>
+    <td> 
+        <form action="updateuser.php" method="POST">
+            <input type="hidden" name="user" value="<?php  echo$row[0] ?>">
+            <input type="submit" value="update">
+        </form> 
+    </td>
+    </tr>
 <?php
 }
 ?>
+</table>
