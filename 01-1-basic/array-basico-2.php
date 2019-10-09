8fade6c4f9869518f456fcd006ea24963f28a4b4
Aitor Agirre <AAgirre@uni.lan>
2019-10-09 12:27:37 +0200

    2 dimentsio array loop

--- 01-1-basic/array-basico-2.php
+++ 01-1-basic/array-basico-2.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "<h3> 1 Dimentsioko array-ak </h3>";
+
 $v = [1,5,6,5,77,88,333,444,66,0];
 
 //$suma = $v[0] + $v[1] + $v[2];
@@ -16,7 +18,7 @@ for ($i = 0; $i < count($v); $i++) {
     //echo "<br>";
 }
 
-echo "batura for: " . $suma . "<br><br>";
+echo "batura for: " . $suma . "<br>";
 
 // while
 $suma = 0;
@@ -37,6 +39,54 @@ foreach ($v as $x) {
 
 echo "batura foreach: " . $suma;
 
+
+/************************
+
+2 dimentsiotako arrayak
+
+*/
+
+echo "<h3> 2 Dimentsioko array-ak </h3>";
+
+$vv = [[1,2,3],[4,5,6]];
+
+// for
+$suma = 0;
+for ($i=0; $i<count($vv); $i++) {
+    for ($j=0; $j<count($vv[0]); $j++) {
+        //echo $vv[$i][$j];
+        $suma = $suma + $vv[$i][$j];
+    }
+}
+
+echo "batura for: " . $suma . "<br>";
+
+// while
+$suma = 0;
+$i=0;
+$j=0;
+while ($i<count($vv)) {
+    $j=0;
+    while ($j<count($vv[0])) {
+        //echo $vv[$i][$j];
+        $suma = $suma + $vv[$i][$j];
+        $j++;
+    }
+    $i++;
+}
+
+echo "batura while: " . $suma . "<br>";
+
+
+// foreach
+$suma = 0;
+foreach ($vv as $v) {
+    foreach($v as $x) {
+        $suma = $suma + $x;
+    }
+}
+
+echo "batura foreach: " . $suma;
 
 
 
