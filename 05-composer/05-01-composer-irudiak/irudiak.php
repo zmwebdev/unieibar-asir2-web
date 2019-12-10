1eced093a2e8080a696543302bd5452b9f0d93f7
zmwebdev <zmwebdev@email.email>
2019-12-10 08:52:34 +0000

    irudiak

--- 05-composer/05-01-composer-irudiak/irudiak.php
+++ 05-composer/05-01-composer-irudiak/irudiak.php
@@ -13,19 +13,37 @@ Image::configure(array('driver' => 'imagick'));
 // and you are ready to go ...
 $image = Image::make('eibar.jpg')->resize(300, 200);
 
-// finally we save the image as a new file
-$image->save('eibar2.jpg');
+//// finally we save the image as a new file
+//$image->save('eibar2.jpg');
 //
-echo "<img src=eibar.jpg>";
-echo "<img src=eibar2.jpg>";
+$image->encode('png');
+$type = 'png';
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($image);
 
-/*
-// import the Intervention Image Manager Class
-use Intervention\Image\ImageManager;
+echo "<img src=$base64>";
 
-// create an image manager instance with favored driver
-$manager = new ImageManager(array('driver' => 'imagick'));
+//*********** */
+// resize the image to a width of 300 and constrain aspect ratio (auto height)
+$image = Image::make('eibar.jpg');
+$image->resize(300, null, function ($constraint) {
+    $constraint->aspectRatio();
+});
+$image->greyscale();
 
-// to finally create image instances
-$image = $manager->make('eibar.jpg')->resize(300, 200);
-*/
+
+$image->encode('png');
+$type = 'png';
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($image);
+
+echo "<img src=$base64>";
+
+//*********** */
+$image = Image::make('eibar.jpg');
+// rotate image 45 degrees clockwise
+$image->rotate(-45);
+
+$image->encode('png');
+$type = 'png';
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($image);
+
+echo "<img src=$base64>";
